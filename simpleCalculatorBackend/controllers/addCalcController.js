b27cfd059673671460323375f75c075c40e20983
bharathib03 <bharathib641@gmail.com>
2025-01-26 04:58:13 +0530

    added business logic

--- simpleCalculatorBackend/controllers/addCalcController.js
+++ simpleCalculatorBackend/controllers/addCalcController.js
@@ -1,10 +1,8 @@
 import { addCalcService } from '../services/addCalcService.js';
 export const addController = async (req, res) => {
-    console.log("Request is ",req.body)
     try {
       const userData = req.body.value;
       const addOutput = await addCalcService(userData);
-      console.log("Otput in controller is ",addOutput)
       res.status(201).send({ result: addOutput });
     } catch (error) {
       res.status(500).send({ message: 'Error creating user', error: error.message });
--- simpleCalculatorBackend/services/addCalcService.js
+++ simpleCalculatorBackend/services/addCalcService.js
@@ -1,4 +1,13 @@
 export const addCalcService = async (userData) => {
-    console.log("User data is ",userData)
-    return userData;
+    //Business logic to remove backslashes, delimiters, and new lines  
+    userData = userData.replace(/[/\\*]/g, "").replace(/[^\w\s]/gi, '').replace(/[^\x20-\x7E]/gmi, '').trim()
+
+    //After removing all delimiters, backslashes, and new lines trim it and convert it to array
+    let newUserArray = userData.split('');
+
+    //Reduce function to sum all the elements in an array
+    let total = newUserArray.reduce(function(sum, value) {
+      return sum + (+value);
+  }, 0);
+    return total;
   };
