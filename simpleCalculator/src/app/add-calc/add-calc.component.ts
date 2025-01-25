3af69020adce008c8e145701a69929a26552158e
bharathib03 <bharathib641@gmail.com>
2025-01-25 23:58:56 +0530

    Initial UI is done

--- simpleCalculator/src/app/add-calc/add-calc.component.ts
+++ simpleCalculator/src/app/add-calc/add-calc.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-calc',
   templateUrl: './add-calc.component.html',
-  styleUrls: ['./add-calc.component.css']
+  styleUrls: ['./add-calc.component.css'],
+
 })
 export class AddCalcComponent implements OnInit {
+  inputNum : string = '';
 
   constructor() { }
-
+ 
   ngOnInit(): void {
+    
+  }
+
+  add(){
+    console.log("Add is clicked")
+    console.log("Val is ",this.inputNum)
   }
 
 }
--- simpleCalculator/src/app/app.module.ts
+++ simpleCalculator/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCalcComponent } from './add-calc/add-calc.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AddCalcComponent } from './add-calc/add-calc.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
