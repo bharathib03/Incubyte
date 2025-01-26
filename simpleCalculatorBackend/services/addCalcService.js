34290b726434788d5a9eca615e1df45962849142
bharathib03 <bharathib641@gmail.com>
2025-01-26 05:35:50 +0530

    Added logic for negative number

--- simpleCalculatorBackend/services/addCalcService.js
+++ simpleCalculatorBackend/services/addCalcService.js
@@ -1,13 +1,34 @@
 export const addCalcService = async (userData) => {
-    //Business logic to remove backslashes, delimiters, and new lines  
-    userData = userData.replace(/[/\\*]/g, "").replace(/[^\w\s]/gi, '').replace(/[^\x20-\x7E]/gmi, '').trim()
+    const negativeCheck = getNegativeNumbers(userData);
 
-    //After removing all delimiters, backslashes, and new lines trim it and convert it to array
-    let newUserArray = userData.split('');
+    if(negativeCheck != null){
+      return  `negative numbers not allowed ${negativeCheck}`
+    }
+    else{
+        //Business logic to remove backslashes, delimiters, and new lines  
+        userData = userData.replace(/[/\\*]/g, "").replace(/[^\w\s]/gi, '').replace(/[^\x20-\x7E]/gmi, '').trim();
 
-    //Reduce function to sum all the elements in an array
-    let total = newUserArray.reduce(function(sum, value) {
-      return sum + (+value);
-  }, 0);
-    return total;
+        //After removing all delimiters, backslashes, and new lines trim it and convert it to array
+        let newUserArray = userData.split('');
+        //Reduce function to sum all the elements in an array
+        let total = newUserArray.reduce(function(sum, value) {
+          return sum + (+value);
+          }, 0);
+        return total;
+    }
   };
+
+  function getNegativeNumbers(input){
+    const regex = /-?\d+/g;
+
+    const matches = input.match(regex);
+    if (matches) {
+      for (const match of matches) {
+        const number = parseInt(match, 10);
+        if (number < 0) {
+          return number;
+        }
+      }
+    }
+    return null;
+  }
