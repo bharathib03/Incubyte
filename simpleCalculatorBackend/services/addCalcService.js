80218da8eb118bafd9e5cea596c0f06e3f9f0aea
bharathib03 <bharathib641@gmail.com>
2025-01-26 05:38:53 +0530

    Added new function

--- simpleCalculatorBackend/services/addCalcService.js
+++ simpleCalculatorBackend/services/addCalcService.js
@@ -5,17 +5,8 @@ export const addCalcService = async (userData) => {
       return  `negative numbers not allowed ${negativeCheck}`
     }
     else{
-        //Business logic to remove backslashes, delimiters, and new lines  
-        userData = userData.replace(/[/\\*]/g, "").replace(/[^\w\s]/gi, '').replace(/[^\x20-\x7E]/gmi, '').trim();
-
-        //After removing all delimiters, backslashes, and new lines trim it and convert it to array
-        let newUserArray = userData.split('');
-        //Reduce function to sum all the elements in an array
-        let total = newUserArray.reduce(function(sum, value) {
-          return sum + (+value);
-          }, 0);
-        return total;
-    }
+      const total = sumOfAllNumbers(userData);
+      return total;
   };
 
   function getNegativeNumbers(input){
@@ -31,4 +22,19 @@ export const addCalcService = async (userData) => {
       }
     }
     return null;
+  }
+
+  function sumOfAllNumbers(input){
+        //Business logic to remove backslashes, delimiters, and new lines  
+        input = input.replace(/[/\\*]/g, "").replace(/[^\w\s]/gi, '').replace(/[^\x20-\x7E]/gmi, '').trim();
+
+        //After removing all delimiters, backslashes, and new lines trim it and convert it to array
+        let newUserArray = input.split('');
+        //Reduce function to sum all the elements in an array
+        let total = newUserArray.reduce(function(sum, value) {
+          return sum + (+value);
+          }, 0);
+        return total;
+  }
+
   }
